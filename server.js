b73645f401010d844259f12b5a26c895bb252b5f
tejaskarale <karaletejas1998@gmail.com>
2018-03-31 16:33:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article1', function (req, res) {
+    res.send('hey do u want article 1 to read?');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
